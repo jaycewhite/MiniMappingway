0f7859ec96d1b6df0365e59f0522e8e32c742c74
jaycewhite <106924902+jaycewhite@users.noreply.github.com>
2022-09-10 17:05:02 +0100

    left debug value on

--- Service/FinderService.cs
+++ Service/FinderService.cs
@@ -83,7 +83,7 @@ namespace MiniMappingway.Service
                     {
 
 
-                        if (((StatusFlags)charPointer->StatusFlags).HasFlag(StatusFlags.OffhandOut))
+                        if (((StatusFlags)charPointer->StatusFlags).HasFlag(StatusFlags.IsCasting))
                         {
                             lock (friends)
                             {
